1973e631846d1d4518d28abaf1eaf914f18b4832
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-11-21 19:17:46 +0200

    Register Validations + Notification

--- client/src/components/User/Register/Register.jsx
+++ client/src/components/User/Register/Register.jsx
@@ -1,29 +1,72 @@
-import SinglePageHead from "../../SinglePageHead/SingePageHead";
-import * as userService from "../../../services/userService";
-import { useAuthContext, withAuth } from "../../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-
+import {  useState } from "react";
+import * as userService from "../../../services/userService";
+import { validateEmail, validateUrl } from "../../../services/userService";
+import { useAuthContext, withAuth } from "../../../context/AuthContext";
+import SinglePageHead from "../../SinglePageHead/SingePageHead";
+import Notification from "../Notication/Notification";
 
 import "./Register.css"
 
-
 const Register = ({ auth }) => {
 
+  //Notification handler
+  let errors = []
+  let success = [`You have successfully registered! Redirecting in 3, 2, 1...`]
+  const initialNotificationState = {type:'', message: []}
 
+  const [notification, setNotification] = useState(initialNotificationState)
+	const [showNotification, setShowNotification] = useState(false);
+
+  const closeNotification = () => {
+    setShowNotification(false)
+    setNotification(initialNotificationState)
+  }
+
+  //Login Logic
   const { isAuthenticated } = useAuthContext();
-
   const navigate = useNavigate();
 
   const onRegister = async (e) => {
     e.preventDefault()
 
-    
-
     const formData = new FormData(e.target);
 
     const { username, email, first_name, last_name, password, re_password, user_imageUrl } = Object.fromEntries(formData)
 
-    //validations TODO!
+    //Validations
+
+    if (username.toString().length < 3 || username.toString().length > 15) {
+			errors.push('Username must be between 3 and 15 characters.')
+		}
+    if ((first_name.toString().length < 3 || first_name.toString().length > 15) || (last_name.toString().length < 3 ||last_name.toString().length > 15)) {
+			errors.push('Both your first and last name must be between 3 and 15 characters.')
+		}
+    if (password.toString().length < 5 || password.toString().length > 10) {
+			errors.push('Password must be between 5 and 10 characters.')
+		}
+    if (password!==re_password) {
+			errors.push('Passwords do not match.')
+		}
+
+    let imageUrlValid = validateUrl(user_imageUrl);
+    if (! imageUrlValid) {
+			errors.push('Image URL must be a valid URL.')
+		}
+
+    let emailValid = validateEmail(email);
+    if (! emailValid) {
+			errors.push('Enter a valid email address.')
+		}
+
+    if(errors.length) {
+			setShowNotification(true)
+			setNotification({
+				type:'error',
+				message: errors
+			});
+      return;
+		}
 
     const userData = {
       username,
@@ -36,27 +79,32 @@ const Register = ({ auth }) => {
     }
 
     try {
-
-      //const response = await register(userData);
-
-      userService.register(userData)
-        .then(authData => {
-          auth.userLogin(authData)
-          navigate("/");
-        });
-
+      const authData = await userService.register(userData);
+      auth.userLogin(authData);
+    
+      setShowNotification(true)
+				setNotification({
+					type:'success',
+					message: success
+				});
+        setTimeout(() => {navigate("/")}, 2000);       
     } catch (err) {
-      console.log(`I received this error on registration: ${err.message}`)
-    }
-  };
+        errors.push(err.message)
+        setShowNotification(true)
+        setNotification({
+        type:'error',
+        message: errors
+       });
+    };
+};
 
   return (
 
     <>
       <SinglePageHead pageInfo={{ name: 'Register', slug: 'register' }} />
-
       <div className="container-register">
         <div className="title sign">Registration</div>
+        {showNotification==true ? <Notification type={notification.type} message={notification.message} closeNotification={closeNotification}  /> : '' }
         <div className="content">
           <form method="POST" onSubmit={onRegister}>
             <div className="user-details">
@@ -97,8 +145,8 @@ const Register = ({ auth }) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 const RegisterWithAuth = withAuth(Register);
 
